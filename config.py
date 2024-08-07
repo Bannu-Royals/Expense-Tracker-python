915af37b54943c07b4c61f68f31d1754372bad80
Durga Prasad Kakileti <durgaprasadkakileti@gmail.com>
2024-08-07 18:51:04 +0530

    commit

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MONGO_URI = os.getenv('MONGO_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://madhavi:ammu1509@cluster1.mtgz6wq.mongodb.net/expense_tracker?retryWrites=true&w=majority')
